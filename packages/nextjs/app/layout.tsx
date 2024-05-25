78a54115cae79f53d89ae28f5f3701ae3bb633be
Antonio Téllez Vergara <antoniotellezvergara@MacBook-Air-de-Antonio-3.local>
2024-05-25 12:05:08 -0600

    cambio de titulo de la pagina

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -5,8 +5,8 @@ import "~~/styles/globals.css";
 import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
 export const metadata = getMetadata({
-  title: "Scaffold-ETH 2 App",
-  description: "Built with 🏗 Scaffold-ETH 2",
+  title: "OBS3DIAN",
+  description: "Built for HACK FS 2024",
 });
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
