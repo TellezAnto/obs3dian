c0c813cc47093e2d4545296faffb76c4858f9c4a
D9J9V <macbookair@MACOS-D9J9V.local>
2024-05-25 13:17:46 -0600

    Upload not working for now

--- packages/nextjs/app/upload/page.tsx
+++ packages/nextjs/app/upload/page.tsx
@@ -1,8 +1,10 @@
 const UploadFiles = () => {
   return (
-    <div className="flex flex-col justify-center items-center py-32">
-      <button className="btn btn-error">Subir archivos</button>
-      <textarea placeholder="Bio" className="textarea textarea-bordered textarea-lg w-full max-w-xs"></textarea>
+    <div
+      className="flex flex-col justify-center items-center"
+      style={{ height: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}
+    >
+      <button className="btn btn-error">Upload Markdown Files</button>
     </div>
   );
 };
