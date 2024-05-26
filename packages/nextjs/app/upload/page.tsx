327e728cf88a01d0b7cd3ec79a77c951ab13d1d9
Antonio Téllez Vergara <antoniotellezvergara@MacBook-Air-de-Antonio-3.local>
2024-05-25 23:14:14 -0600

    limpiando página principal

--- packages/nextjs/app/upload/page.tsx
+++ packages/nextjs/app/upload/page.tsx
@@ -1,10 +1,13 @@
 const UploadFiles = () => {
   return (
-    <div
-      className="flex flex-col justify-center items-center"
-      style={{ height: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}
-    >
-      <button className="btn btn-error">Upload Markdown Files</button>
+    <div>
+      <div
+        className="flex flex-col justify-center items-center w-full h-screen bg-secondary "
+        style={{ height: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}
+      >
+        <button className="btn btn-error ">Upload Markdown Files</button>
+        <input type="file" className="file-input file-input-bordered file-input-primary w-full max-w-xs mt-10 " />
+      </div>
     </div>
   );
 };
