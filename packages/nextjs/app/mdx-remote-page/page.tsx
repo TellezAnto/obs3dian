219504054ebd181700303fe63ad095df8bfa1cab
D9J9V <macbookair@MACOS-D9J9V.local>
2024-05-25 19:37:28 -0600

    Remote origin works now from IPFS

--- packages/nextjs/app/mdx-remote-page/page.tsx
+++ packages/nextjs/app/mdx-remote-page/page.tsx
@@ -2,7 +2,7 @@ import { MDXRemote } from "next-mdx-remote/rsc";
 
 export default async function RemoteMdxPage() {
   // MDX text - can be from a local file, database, CMS, fetch, anywhere...
-  const res = await fetch("https://ipfs.io/ipns/k51qzi5uqu5dlwwdpp7xef1wra8786l87wnpjlg1usfhxy4vi5bp3ifzid0thg");
+  const res = await fetch("https://ipfs.io/ipfs/QmR9gpQDipNgh7Q1JFudUzj3gzKDREwwHqr2eboAky2frT?filename=hola.md");
   const markdown = await res.text();
   return <MDXRemote source={markdown} />;
 }
