f25e000808f6035858355258221d50f0c3a0b3c5
D9J9V <macbookair@MACOS-D9J9V.local>
2024-06-01 13:32:57 -0600

    Debugging remoteMDX

--- packages/nextjs/app/mdx-remote-page/page.tsx
+++ packages/nextjs/app/mdx-remote-page/page.tsx
@@ -2,8 +2,8 @@
 
 export default async function RemoteMdxPage() {
   // MDX text - can be from a local file, database, CMS, fetch, anywhere...
-  const res = await fetch("https://ipfs.io/ipfs/QmR9gpQDipNgh7Q1JFudUzj3gzKDREwwHqr2eboAky2frT?filename=hola.md");
-  const markdown = await res.text();
+  //const res = await fetch("https://ipfs.io/ipfs/QmR9gpQDipNgh7Q1JFudUzj3gzKDREwwHqr2eboAky2frT?filename=hola.md");
+  //const markdown = await res.text();
   //return <MDXRemote source={markdown} />;
-  return markdown;
+  return <p>hello-world</p>;
 }
